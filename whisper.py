d43d4c9d949b74d59c22e32c508b9348e40149c6
AntigoneJ <jennifer.joey@hotmail.ch>
2026-05-09 00:45:06 +0200

    Configured whisper to transcribe german text, sequential for now

--- whisper.py
+++ whisper.py
@@ -1,5 +1,8 @@
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+import json
+import pandas as pd
+import os
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 print(device)
@@ -8,7 +11,7 @@ torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 model_id = "openai/whisper-large-v3-turbo"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    model_id, dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
 )
 model.to(device)
 
@@ -19,11 +22,36 @@ pipe = pipeline(
     model=model,
     tokenizer=processor.tokenizer,
     feature_extractor=processor.feature_extractor,
-    torch_dtype=torch_dtype,
+    dtype=torch_dtype,
     device=device,
 )
 
-sample = "mp3/petra__session__wir-sollten-zun-chst-eine-bestandsaufnahme-ihrer-website-durchf-hren__2026-05-04t17-10-05-828z.mp3"
+# go over all samples in the mp3 folder and transcribe them with Whisper, save the result in a jsonl file with the file name and the transcription
+files = os.listdir("mp3")
+# Load the jsonl file
+df = pd.read_json("transcriptions.jsonl", lines=True)
 
-result = pipe(sample)
-print(result["text"])
+# Save the result in a jsonl file with the file name and the transcription
+# Check that the file_name is not already in the jsonl file to prevent duplicates
+
+for filename in files:
+    if filename.endswith(".mp3"):
+        sample = os.path.join("mp3/", filename)
+        result = pipe(
+            sample,
+            generate_kwargs={
+                "language": "german",
+                "task": "transcribe",
+            },  # force german and transcribe task
+        )
+        # Get the keys (columns)
+    if "file_name" in df.columns:
+        # Check if the file_name is already in the jsonl file
+        if sample not in df["file_name"].values:
+            entry = {"file_name": sample, "whisper_output": result["text"]}
+        else:
+            print(f"File {sample} is already in the jsonl file.")
+    else:
+        entry = {"file_name": sample, "whisper_output": result["text"]}
+        with open("transcriptions.jsonl", "a") as f:
+            f.write(json.dumps(entry) + "\n")
